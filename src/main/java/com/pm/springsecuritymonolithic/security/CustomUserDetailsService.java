f1b553397712515c9857accd5471b8b99515d38c
sparsh-DBALounge <sparsh.kadian@dbalounge.com>
2025-06-26 20:59:50 +0530

    implement UserDetails and UserDetailsService

---
+++ src/main/java/com/pm/springsecuritymonolithic/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.pm.springsecuritymonolithic.security;
+
+import com.pm.springsecuritymonolithic.model.User;
+import com.pm.springsecuritymonolithic.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByUsername(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email (" + email + ") not Found"));
+
+        return new CustomUserDetails(user);
+    }
+}
