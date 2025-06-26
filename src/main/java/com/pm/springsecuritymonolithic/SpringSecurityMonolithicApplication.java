e63a5a717b1f8aeff3b8d4375d155b4893cc3c4a
sparsh-DBALounge <sparsh.kadian@dbalounge.com>
2025-06-26 20:33:24 +0530

    connect to database

---
+++ src/main/java/com/pm/springsecuritymonolithic/SpringSecurityMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.pm.springsecuritymonolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMonolithicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMonolithicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/springsecuritymonolithic/SpringSecurityMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.springsecuritymonolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
